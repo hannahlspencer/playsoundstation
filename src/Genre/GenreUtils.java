04503510e033b66be936a002388ba176e7759992
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-04 19:37:47 +0100

    improving genreutils
    Cutting down on repeated code for each genre class by using utils instead

--- src/Genre/GenreUtils.java
+++ src/Genre/GenreUtils.java
@@ -5,26 +5,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenreUtils {
-    public static String getInstrument(Genre x) {
-        String y = x.getClass().toString();
-        String ins = y + "Instruments";
-        ins = ins.replace("class ", "");
 
+    static List instruments;
+    static String genre;
+
+    public static void setGenre(Genre x) {
+        String y = x.getClass().toString();
+        String ins = y.replace("class ", "");
+        genre = ins;
+    }
+
+    public static void getInstruments() {
+        String ins = genre + "Instruments";
         try {
-            Class<?> instruments = Class.forName(ins);
+            Class<?> selection = Class.forName(ins);
             List list = new ArrayList();
-            if(instruments.isEnum()) {
-                list = Arrays.asList(instruments.getEnumConstants());
+            if(selection.isEnum()) {
+                list = Arrays.asList(selection.getEnumConstants());
             }
-             Object instrument =
-                     list.get((int)(Math.random()*list.size()));
-            return "[" + instrument + "] ";
+             instruments = list;
         } catch (ClassNotFoundException e) {
             System.out.println("There is not a genre of that type stored with instruments");
             e.printStackTrace();
         }
 
-        return y;
+    }
 
+    public static String getRandomInstrument() {
+        return instruments.get((int) (Math.random()*instruments.size())).toString();
     }
 }
