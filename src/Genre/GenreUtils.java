d9bd758602335c46b97f1399523a83cad7485a2c
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-05 16:22:03 +0100

    Add documentation to GenreUtils

--- src/Genre/GenreUtils.java
+++ src/Genre/GenreUtils.java
@@ -9,26 +9,54 @@ public class GenreUtils {
     private static String[] instruments;
     private static Genre genreClass;
 
-    public static void setGenre(Genre x) {
-        genreClass = x;
+    /**
+     * Takes the genre of the piece of music being build and saves it so that other
+     * methods can change what they're doing based on the information within that specific genre class
+     * @param genre
+     */
+    public static void setGenre(Genre genre) {
+        genreClass = genre;
     }
 
+    /**
+     *
+     * @return the class of the Genre being used
+     */
     public static Genre getGenre() {
         return genreClass;
     }
 
-    public static void getInstruments() {
+    /**
+     * saves the instruments that can be used from the Genre class being considered
+     */
+    public static void setInstruments() {
         instruments = genreClass.getInstruments();
     }
 
+    /**
+     *
+     * @return the instruments that have been saved from the Genre class
+     */
     public static String[] returnInstruments() {
         return instruments;
     }
+
+    /**
+     * From the instruments that have been saved from the genre class, selects one at random to
+     * use for the line of melody being considered
+     * @return random instrument as a String to use in a String of melody
+     */
 
     public static String getRandomInstrument() {
         return instruments[(int) (Math.random()*instruments.length)].toString();
     }
 
+    /**
+     *  Selects a random Bass instrument to use for the bassline of the piece of music,
+     *  then random selects a series of notes or rests to be used for that bassline
+     * @param numOfNotes
+     * @return String that contains the bass melody and instrument
+     */
     public static String setBassline(int numOfNotes) {
         BassInstruments instrument =
                 BassInstruments.values()[(int)(Math.random()*BassInstruments.values().length)];
