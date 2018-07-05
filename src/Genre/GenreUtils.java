6090ab1b5a526ba989722f84526617cfd176b130
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-05 13:05:54 +0100

    adding setBassline method to GenreUtils

--- src/Genre/GenreUtils.java
+++ src/Genre/GenreUtils.java
@@ -7,16 +7,22 @@ import java.util.List;
 public class GenreUtils {
 
     static List instruments;
-    static String genre;
+    static String genreString;
+    static Genre genreClass;
 
     public static void setGenre(Genre x) {
-        String y = x.getClass().toString();
-        String ins = y.replace("class ", "");
-        genre = ins;
+        genreClass = x;
+        String genre = x.getClass().toString();
+        String cleanGenre = genre.replace("class ", "");
+        genreString = cleanGenre;
+    }
+
+    public static String getGenre() {
+        return genreString;
     }
 
     public static void getInstruments() {
-        String ins = genre + "Instruments";
+        String ins = genreString + "Instruments";
         try {
             Class<?> selection = Class.forName(ins);
             List list = new ArrayList();
@@ -31,7 +37,26 @@ public class GenreUtils {
 
     }
 
+    public static List returnInstruments() {
+        return instruments;
+    }
+
     public static String getRandomInstrument() {
         return instruments.get((int) (Math.random()*instruments.size())).toString();
+    }
+
+    public static String setBassline(int numOfNotes) {
+        BassInstruments instrument =
+                BassInstruments.values()[(int)(Math.random()*BassInstruments.values().length)];
+        String[] noteLengths = genreClass.bassNoteLengths;
+        String[] letters = {"A3", "B3", "C3", "D3", "E3", "F3", "G3", "R3"};
+
+        String melody = "";
+        for(int i = 0; i < numOfNotes; i++) {
+            String note = letters[(int) (Math.random() * letters.length)];
+            String length = noteLengths[(int) (Math.random() * noteLengths.length)];
+            melody += (note + length + " ");
+        }
+        return "I[" + instrument + "] " + melody;
     }
 }
