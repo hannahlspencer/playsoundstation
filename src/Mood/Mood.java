bbc6fb6c3959f8d19ee567fbf7c98f49fd910438
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-06 11:42:27 +0100

    CHanging interface into data holding class

--- src/Mood/Mood.java
+++ src/Mood/Mood.java
@@ -4,22 +4,11 @@ import java.util.ArrayList;
 
 public interface Mood {
 
-    /**
-     * Based on the user input finds a set of music keys that could be used
-     * Picks one of these keys at random and returns an ArrayList of possible notes to choose from
-     * when constructing a piece of music
-     */
-    String setKey();
+    String key = "";
+    int[] tempoRange = new int[0];
 
-    /**
-     * Percussion types and intensity will be decided based on Mood.Mood input.
-     * @return String that can be fed into a player that creates percussive sounds
-     */
-    String addPercussion();
+    String getKey();
 
-    /**
-     * The mood will affect the tempo, eg. sadder moods tend to be slower
-     * @return String that will change the tempo of the piece
-     */
-    String decideTempo();
+    int[] getTempoRange();
+
 }
