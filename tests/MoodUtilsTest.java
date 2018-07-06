93eada82f08c8ed6d980474c393851256dd971f5
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-06 11:41:55 +0100

    adding tests for tempo

--- tests/MoodUtilsTest.java
+++ tests/MoodUtilsTest.java
@@ -1,7 +1,6 @@
 import Mood.*;
 import org.junit.Before;
 import org.junit.Test;
-import Genre.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,13 +10,50 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 public class MoodUtilsTest {
 
+    Mood testMood;
+
     @Before
     public void initialise() {
-        Mood testMood = new Happy();
+        testMood = new Happy();
         MoodUtils.setMood(testMood);
     }
     @Test
-    public void testSetKey() {
-        
+    public void testSetKeyType() {
+        String result = MoodUtils.setKey();
+        assertTrue(result.contains("maj"));
+    }
+
+    @Test
+    public void testSetKeyNote() {
+        String result = MoodUtils.setKey();
+        List test = new ArrayList<Character>();
+        test.add('A');
+        test.add('B');
+        test.add('C');
+        test.add('D');
+        test.add('E');
+        test.add('F');
+        test.add('G');
+        assertTrue(test.contains(result.charAt(0)));
+    }
+
+    @Test
+    public void testAddPercussion() {
+
+    }
+
+    @Test
+    public void testDecideTempoInitial() {
+        String result = MoodUtils.decideTempo();
+        assertEquals('T', result.charAt(0));
+    }
+
+    @Test
+    public void testTempoSpeed() {
+        String result = MoodUtils.decideTempo();
+        result = result.replace("T", "");
+        int numResult =Integer.parseInt(result);
+        int[] range = testMood.getTempoRange();
+        assertTrue((numResult > range[0]) && (numResult < range[1]));
     }
 }
