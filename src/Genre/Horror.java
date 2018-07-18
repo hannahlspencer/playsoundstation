750c1a44b432e1318bb99f547a3e7c62d163b3fe
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-18 14:27:59 +0100

    Change spacing

--- src/Genre/Horror.java
+++ src/Genre/Horror.java
@@ -2,7 +2,7 @@ package Genre;
 
 public class Horror implements Genre{
 
-    String[] bassNoteLengths = { "w"};
+    String[] bassNoteLengths = { "w" };
     String[] instruments = {"VIOLIN", "VIOLA", "CELLO", "CHOIR_OOHS", "CHOIR_AAHS", "SYNTH_VOICE", "CRYSTAL","TUBULAR_BELLS",
             "MUSIC_BOX", "BREATH_NOISE", "METALLIC"};
 
