586ae4543d0ff2fda711ac2c7b11be9764424fdb
Hannah Spencer <hannahlspencer@gmail.com>
2018-07-18 14:27:35 +0100

    Build Score Factory

--- src/Score.java
+++ src/Score.java
@@ -1,12 +1,15 @@
 import Genre.*;
 import Mood.*;
 import Function.*;
+import org.jfugue.pattern.Pattern;
 import org.jfugue.rhythm.Rhythm;
 
 public class Score {
     public Score(Genre g, Mood m) {
         genre = g;
         mood = m;
+        GenreUtils.setGenre(g);
+        MoodUtils.setMood(m);
     }
     //this will increment with each voice added to the sound
     int counter = 0;
@@ -18,17 +21,17 @@ public class Score {
     String melody;
     String key;
     String tempo;
-    Rhythm percussion;
+    Pattern percussion;
 
-    public void initialiseUtils() {
-        GenreUtils.setGenre(genre);
-        MoodUtils.setMood(mood);
-    }
 
     public String startNewVoice() {
-        String result = " V" + counter;
+        String result = " V" + counter + " ";
         counter++;
         return result;
+    }
+
+    public String getScore() {
+        return score;
     }
 
     public void setInstrument() {
@@ -53,17 +56,41 @@ public class Score {
     }
 
     public void initialiseScore() {
-        score = tempo + key + startNewVoice() + instrument;
+        score = tempo + key + startNewVoice() + instrument + makeNewMelody();
+    }
+
+    private String makeNewMelody() {
+        melody = "";
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "R"};
+        String[] noteLengths = { "w", "h", "q", "i" };
+        for (int i = 0; i < 16; i++) {
+            String note = letters[(int) (Math.random() * letters.length)];
+            String length = noteLengths[(int) (Math.random() * noteLengths.length)];
+
+            melody += (note + length + " ");
+        }
+        return melody;
     }
 
     public void addBass() {
-        score = score + startNewVoice() + GenreUtils.setBassline(4);
+        String bassline = GenreUtils.setBassline(16);
+        System.out.println("We have no bassline");
+        if(bassline != "") {
+            System.out.println("We have a bassline!");
+            score = score + startNewVoice() + bassline;
+        }
     }
-    public void getPercussion() {
+    public void setPercussion() {
         percussion = MoodUtils.addPercussion();
     }
 
-    public void updateScore(String newMelody) {
-        score = score + " V" + counter + instrument + " " + melody;
+    public Pattern getPercussion() {
+        return percussion;
+    }
+
+    public void updateScore() {
+        setInstrument();
+
+        score = score + startNewVoice() + instrument + makeNewMelody();
     }
 }
