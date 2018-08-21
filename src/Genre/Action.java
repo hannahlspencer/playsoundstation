b397341d83bd8e25f3906f011c5e930cd469b76c
Hannah Spencer <hannahlspencer@gmail.com>
2018-08-21 15:43:33 +0100

    remove harmonics

--- src/Genre/Action.java
+++ src/Genre/Action.java
@@ -3,7 +3,7 @@ package Genre;
 public class Action implements Genre {
 
     private String[] bassNoteLengths = { "q", "i" };
-    private String[] instruments = {"ROCK_ORGAN", "OVERDRIVEN_GUITAR", "GUITAR_HARMONICS", "DISTORTION_GUITAR"};
+    private String[] instruments = {"ROCK_ORGAN", "OVERDRIVEN_GUITAR", "DISTORTION_GUITAR"};
 
     @Override
     public String[] getBassNoteLengths() {
