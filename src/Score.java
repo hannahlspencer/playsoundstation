17f278c3cd5661dd3313f6dd8a613b1aefb583df
Hannah Spencer <hannahlspencer@gmail.com>
2018-08-25 14:08:39 +0100

    comments added for clarity

--- src/Score.java
+++ src/Score.java
@@ -2,7 +2,6 @@ import Genre.*;
 import Mood.*;
 import org.jfugue.pattern.Pattern;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,21 +17,22 @@ public class Score implements ScoreInterface {
         barsToWrite = bars;
     }
     //this will increment with each voice added to the sound
-    private int counter = 0;
+    private int voiceCounter = 0;
     //holds the overall score so far
     private String score;
+    //counts down how many more bars need to be written
     private int barsToWrite;
+    //stores current main key of the score
     private String key;
-    private List<String> notes = new ArrayList<>();
 
     public int getCounter() {
-        return counter;
+        return voiceCounter;
     }
 
     @Override
     public String startNewVoice() {
-        String result = " V" + counter + " ";
-        counter++;
+        String result = " V" + voiceCounter + " ";
+        voiceCounter++;
         return result;
     }
 
@@ -72,42 +72,55 @@ public class Score implements ScoreInterface {
         int bars = 0;
         //this variable keeps track of the notes in each bar, and a new bar is created whenever this reaches 4
         double barLength = 0;
-        //the for loop selects 16 notes from the pitches and lengths, puts them together and adds them to the melody
         int thirds = barsToWrite/3;
+        //these booleans keep track of whether the key change has happened yet
         Boolean keyChanged = false;
         Boolean keyReturned = false;
+        //writes the actual music
         while (bars < barsToWrite) {
+            //changes key to the V progression a third of the way through the piece
             if (bars == thirds && !keyChanged) {
                 keyChanged = true;
                 melody += keyProgression(true);
             }
+            //returns the piece to the original key two thirds of the way through the piece
             if(bars == thirds * 2 && !keyReturned) {
                 keyReturned = true;
                 melody += keyProgression(false);
             }
+
+            //goes note by note
             String note = notes.get((int) (Math.random() * notes.size()));
             String length;
             //this checks if it's a bass note
             if(note.contains("3")) {
+                //if a bass note, then the appropriate bass note lengths are fetched
                 String[] noteLengths = GenreUtils.getBassNotes();
+
+                // chooses a random note length appropriate to the beats left in the bar
                 length = ScoreUtils.chooseNoteLength(barLength);
                 //this checks that the note returned matches with the note lengths that are appropriate for the Genre being
-                //used. If not, then the note is turned into a rest and the length is chosen according to the bar
+                //used. If not, then the note is turned into a rest
                 if(!Arrays.asList(noteLengths).contains(length)) {
                     note = "R";
-                    length = ScoreUtils.chooseNoteLength(barLength);
                 }
             } else {
+                //if note is not a bass note the length is chosen as normal without consulting anything else
                 length = ScoreUtils.chooseNoteLength(barLength);
             }
+
+            //adds the length of the note just chosen to how many notes are currently in the bar
             barLength += ScoreUtils.checkBar(length);
+            //adds note and length to melody
             melody += (note + length + " ");
             //if the bar length is 4, then a bar line is added, the number of bars incremented, and the
             //length of the bar reset to zero
             if(barLength == 4) {
-                if(bars < (barsToWrite -1)) {
+                //if there are still bars left to write, a bar line is added
+                if(bars < (barsToWrite - 1)) {
                     melody += "| ";
                 }
+                //if this is the final bar, the final note is turned into the tonic
                 if(bars == barsToWrite - 1) {
                     melody = fixFinalNote(melody);
                 }
@@ -119,18 +132,16 @@ public class Score implements ScoreInterface {
     }
 
 
-
-
-
+    @Override
     public String keyProgression(Boolean newKey) {
+        //if the newKey is true then the key needs to be changed to the V progression of the original key
         if(newKey) {
             int count = 4;
-            String keyToChange = key.replaceAll("['KEY:''min''maj'' ''#''b']", "");
-            if(keyToChange.equals("")) {
-                keyToChange = "E";
-            }
-            char progress = keyToChange.charAt(0);
-            int asciiKey = progress;
+            //removes key information to only leave the letter note
+            String keyToChange = key.substring(5, key.length()).replaceAll("['min''maj'' ''#''b']", "");
+
+            int asciiKey = keyToChange.charAt(0);
+            //uses ascii values of the notes to find the fifth next one, looping round if it gets to G/71
             while(count > 0) {
                 if(asciiKey == 71) {
                     asciiKey = 65;
@@ -142,6 +153,7 @@ public class Score implements ScoreInterface {
             char charKey = (char) asciiKey;
             String updatedKey = "KEY:";
             updatedKey += String.valueOf(charKey);
+            //reverses whether the key is minor or major for the progression
             if(key.contains("maj")) {
                 updatedKey += "min ";
             } else {
@@ -150,17 +162,15 @@ public class Score implements ScoreInterface {
 
             return updatedKey;
         } else {
+            //if the key needs changing back, reverts back to the original key stored as a global variable
             return key + " ";
         }
     }
 
+    @Override
     public String fixFinalNote(String melody) {
         //finding the tonic note for the piece from the key signature
-        String tonic = key.replaceAll("['KEY:''min''maj'' ''#''b']", "");
-
-        if(tonic.equals("")){
-            tonic = "E";
-        }
+        String tonic = key.substring(5, key.length()).replaceAll("['min''maj'' ''#''b']", "");
         int noteToMinus = 3;
         if(melody.substring(melody.length() - 3, melody.length()).contains("3")) {
             noteToMinus = 4;
@@ -184,8 +194,8 @@ public class Score implements ScoreInterface {
 
     }
     public Pattern getPercussion() {
-        Pattern drums1 = MoodUtils.addPercussion(barsToWrite).repeat(barsToWrite/4);
-        Pattern drums2 = MoodUtils.addPercussion(barsToWrite).repeat(barsToWrite/4);
+        Pattern drums1 = MoodUtils.addPercussion().repeat(barsToWrite/4);
+        Pattern drums2 = MoodUtils.addPercussion().repeat(barsToWrite/4);
         return new Pattern(drums1, drums2, drums2, drums1);
     }
 
